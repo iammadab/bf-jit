9d2137116e39f5b339b50fa05e58b9a41fa3865a
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2025-12-08 19:28:32 +0100

    perf: matching '[' ']' jump table (#3)
    * impl jump table logic
    
    * fix typo

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,44 @@ fn main() {
         }
     }
 
-    // interpret
     let mut pc = 0;
+
+    // compute jump table
+    // given that the program isn't changing, we can compute
+    // '[' and ']' match pc values once
+    // then amortize across loop iterations
+    let mut jump_table = vec![0; instructions.len()];
+    while pc < instructions.len() {
+        let insn = instructions[pc];
+        if insn == '[' {
+            // find pc for matching ']'
+            let mut bracket_nesting = 1;
+            let mut seek = pc;
+
+            while bracket_nesting > 0 {
+                seek += 1;
+                if seek >= instructions.len() {
+                    panic!("unmatched '[' at pc={}", pc);
+                }
+
+                match instructions[seek] {
+                    ']' => bracket_nesting -= 1,
+                    '[' => bracket_nesting += 1,
+                    _ => {}
+                }
+            }
+
+            // map '[' to ']' and ']' to '['
+            jump_table[pc] = seek;
+            jump_table[seek] = pc;
+        }
+        pc += 1;
+    }
+
+    // reset PC
+    pc = 0;
+
+    // interpret
     let mut data_ptr = 0;
 
     while pc < instructions.len() {
@@ -40,44 +76,14 @@ fn main() {
             // if the current data location is zero
             '[' => {
                 if memory[data_ptr] == 0 {
-                    let mut bracket_nesting = 1;
-                    // pc points to '['
-                    let saved_pc = pc;
-
-                    while bracket_nesting > 0 {
-                        pc += 1;
-                        if pc >= instructions.len() {
-                            panic!("unmatched '[' at pc={}", saved_pc);
-                        }
-
-                        match instructions[pc] {
-                            ']' => bracket_nesting -= 1,
-                            '[' => bracket_nesting += 1,
-                            _ => {}
-                        }
-                    }
+                    pc = jump_table[pc];
                 }
             }
             // jumps to the matching '['
             // if the current data location is not zero
             ']' => {
                 if memory[data_ptr] != 0 {
-                    let mut bracket_nesting = 1;
-                    // pc points to ']'
-                    let saved_pc = pc;
-
-                    while bracket_nesting > 0 && pc > 0 {
-                        pc -= 1;
-                        match instructions[pc] {
-                            '[' => bracket_nesting -= 1,
-                            ']' => bracket_nesting += 1,
-                            _ => {}
-                        }
-                    }
-
-                    if bracket_nesting != 0 {
-                        panic!("unmatched ']' at pc={}", saved_pc);
-                    }
+                    pc = jump_table[pc];
                 }
             }
             _ => {}
