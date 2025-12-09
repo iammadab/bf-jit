306321af4f5f6ef89d911126222a59de3d440a22
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2025-12-09 14:00:30 +0100

    add tracing logic behind feature (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::{fs, io::Read};
 
+#[cfg(feature = "tracing")]
+use std::collections::HashMap;
+
 const MEMORY_SIZE: usize = 30_000;
 
 fn main() {
-    let mut memory = [0; MEMORY_SIZE];
+    let mut memory = [0_u8; MEMORY_SIZE];
 
     let args = std::env::args().collect::<Vec<String>>();
     let program = fs::read_to_string(&args[1]).unwrap();
 
     let mut instructions = Vec::with_capacity(program.len());
+
+    #[cfg(feature = "tracing")]
+    let mut insn_count = HashMap::new();
 
     for c in program.chars() {
         match c {
@@ -59,15 +65,19 @@ fn main() {
 
     while pc < instructions.len() {
         let insn = instructions[pc];
+
+        #[cfg(feature = "tracing")]
+        insn_count.entry(insn).and_modify(|v| *v += 1).or_insert(1);
+
         match insn {
             // advance the data ptr to the right by 1
             '>' => data_ptr += 1,
             // advance the data ptr to the left by 1
             '<' => data_ptr -= 1,
             // increment the memory slot at the data ptr
-            '+' => memory[data_ptr] += 1,
+            '+' => memory[data_ptr] = memory[data_ptr].wrapping_add(1),
             // decrement the memory slot at the data ptr
-            '-' => memory[data_ptr] -= 1,
+            '-' => memory[data_ptr] = memory[data_ptr].wrapping_sub(1),
             // print the content of the data ptr to stdout
             '.' => print!("{}", memory[data_ptr] as char),
             // read from stdin and write to memory slot at data ptr
@@ -91,6 +101,15 @@ fn main() {
 
         pc += 1;
     }
+
+    #[cfg(feature = "tracing")]
+    {
+        // print tracing report
+        for (k, v) in &insn_count {
+            println!("{} -> {}", k, comma_format(*v));
+        }
+        println!("Total: {}", comma_format(insn_count.values().sum::<u64>()));
+    }
 }
 
 fn read_byte() -> u8 {
@@ -99,4 +118,17 @@ fn read_byte() -> u8 {
         Ok(1) => buf[0],
         _ => 0,
     }
+}
+
+#[cfg(feature = "tracing")]
+fn comma_format(n: u64) -> String {
+    let s = n.to_string();
+    let mut out = vec![];
+    for (i, c) in s.chars().rev().enumerate() {
+        if i != 0 && i % 3 == 0 {
+            out.push(',');
+        }
+        out.push(c);
+    }
+    out.into_iter().rev().collect()
 }
