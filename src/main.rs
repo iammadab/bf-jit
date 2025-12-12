b8b54f20d5092c2fa597e9bb44b64abd7b4dfc23
Wisdom Ogwu <iammadab@gmail.com>
2025-12-12 18:53:24 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,3 @@ fn main() {
     let program = Program::from_source(source);
     interpret(&program);
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn loop_optimization() {
-        let program = Program::from_source(String::from("[-]"));
-        assert_eq!(program.instructions.len(), 1);
-        assert_eq!(program.instructions[0], Opcode::LoopSetToZero);
-
-        let program = Program::from_source(String::from("[>>]"));
-        assert_eq!(program.instructions.len(), 1);
-        assert_eq!(program.instructions[0], Opcode::LoopMovePtr(2, true));
-
-        let program = Program::from_source(String::from("[->>>+<<<]"));
-        assert_eq!(program.instructions.len(), 1);
-        assert_eq!(program.instructions[0], Opcode::LoopMoveData(3, true));
-
-        let program = Program::from_source(String::from(">>>[-<<<<<<+>>>>>>]"));
-        assert_eq!(program.instructions.len(), 2);
-        assert_eq!(program.instructions[0], Opcode::IncPtr(3));
-        assert_eq!(program.instructions[1], Opcode::LoopMoveData(6, false));
-    }
-}
