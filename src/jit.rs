29260eedb1c476d5c9b51a3a357fa3e69086d86d
Wisdom Ogwu <iammadab@gmail.com>
2025-12-15 07:03:01 +0100

    define compile

---
+++ src/jit.rs
@@ -0,0 +1,43 @@
+// I want to implement jit compilation for brainfuck
+// first I'd do hand implementation, then I'd use cranelift
+// and potentially LLVM
+
+// let us look into hand implementations
+// I have the following opcodes
+// IncPtr(u8)
+// DecPtr(u8)
+// IncData(u8)
+// DecData(u8)
+// ReadStin
+// WriteStdout
+// LoopSetToZero
+// LoopMovePtr(u8, bool)
+// LoopMoveData(u8, bool)
+// JumpIfDataZero(usize)
+// JumpIfDataNotZero(usize)
+//
+// I need to replicate the model in an x86 system
+// we need contiguous memory (we have this)
+// we start at some address (this represents the data pointer address)
+// IncPtr and DecPtr will move this address back and forth
+// IncData and DecData will update the content of the register
+// WriteStdout and ReadStdin should be possible with system V calls
+// LoopSetToZero just sets the current data to zero
+// LoopMovePtr depending on the bool we either + or -
+//  think there should be ways to do branchless compilation here
+// LoopMoveData similar to the one above but with the actualy data not the pointer
+// JumpIfDataZero
+//  we can use a compare then jump appropriately
+// JumpIfDataNotZero
+//  similar as above
+
+// How do I handle the Jump instructions
+// my parser already has the position of the next jump instruction
+// but when jumping in assembly the position changes
+// I think I can also use a stack
+
+use crate::parser::Program;
+
+fn compile(program: &Program) -> &[u8] {
+    todo!()
+}
